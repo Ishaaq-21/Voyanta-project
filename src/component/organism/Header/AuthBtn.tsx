8b28561b8a030e2d2d8577f39b147d1025ab6a28
isaac <ishak.hadj14@gmail.com>
2025-08-31 21:33:00 +0100

    add small prop to the button to adjust the size dynamically

--- src/component/organism/Header/AuthBtn.tsx
+++ src/component/organism/Header/AuthBtn.tsx
@@ -2,17 +2,17 @@ import MainButton from "@/component/atoms/MainButton";
 import Link from "next/link";
 import { JSX } from "react";
 
-const LoginBtn = (): JSX.Element => {
+const LoginBtn = ({ small }: { small: boolean }): JSX.Element => {
   return (
     <Link href={"/sign-in"}>
-      <MainButton buttonName="Login" />
+      <MainButton buttonName="Login" small={small} />
     </Link>
   );
 };
-const SingUpBtn = (): JSX.Element => {
+const SingUpBtn = ({ small }: { small: boolean }): JSX.Element => {
   return (
     <Link href={"/sign-up"}>
-      <MainButton buttonName="Sign-up" />
+      <MainButton buttonName="Sign-up" small={small} />
     </Link>
   );
 };
