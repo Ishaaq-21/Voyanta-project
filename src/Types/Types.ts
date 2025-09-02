2c3d0f827b0776b3d35c2882ed084e85672948d3
isaac <ishak.hadj14@gmail.com>
2025-09-02 15:04:48 +0100

    Create the main objects types for our app

---
+++ src/Types/Types.ts
@@ -0,0 +1,64 @@
+export type Reviews = {
+  _id: string;
+  review: string;
+  rating: number;
+  userId: "string";
+  tourId: string;
+};
+
+export interface TourSimple {
+  id: number;
+  name: string;
+  duration: number;
+  maxGroupSize: number;
+  difficulty: string;
+  ratingsAverage: number;
+  ratingsQuantity: number;
+  price: number;
+  summary: string;
+  description: string;
+  imageCover: string;
+  images: string[];
+  startDates: string[];
+}
+interface Location {
+  _id: string;
+  description: string;
+  type: string;
+  coordinates: [number, number];
+  day: number;
+}
+
+interface StartLocation {
+  description: string;
+  type: string;
+  coordinates: [number, number];
+  address: string;
+}
+export interface Tour {
+  startLocation: StartLocation;
+  ratingsAverage: number;
+  ratingsQuantity: number;
+  images: string[];
+  startDates: string[];
+  _id: string;
+  name: string;
+  duration: number;
+  maxGroupSize: number;
+  difficulty: string;
+  guides: string[];
+  price: number;
+  summary: string;
+  description: string;
+  imageCover: string;
+  locations: Location[];
+}
+export interface User {
+  _id: string;
+  name: string;
+  email: string;
+  role: string;
+  active: boolean;
+  photo: string;
+  password: string;
+}
