f3dd59a9c256fd708489e6b688e3a5c21f2d1a8f
isaac <ishak.hadj14@gmail.com>
2025-09-03 21:47:43 +0100

    refactor: convert the tourList compo to a ssc

--- src/component/molecules/ToursList.tsx
+++ src/component/molecules/ToursList.tsx
@@ -1,26 +1,16 @@
-"use client";
-import { useEffect, useState } from "react";
 import Container from "./Container";
 import { TourSimple } from "@/Types/Types";
 import { getAllSimpleTravels } from "@/_lib/apiClient";
 import TourCard from "./TourCard/TourCard";
-import { v4 as uuidv4 } from "uuid";
-const TourList = () => {
-  const [tourList, setTourList] = useState<TourSimple[]>([]);
 
-  useEffect(() => {
-    const fetchTours = async () => {
-      const toursData = await getAllSimpleTravels();
-      setTourList(toursData);
-    };
-    fetchTours();
-  }, []);
+const TourList = async () => {
+  const toursList: TourSimple[] = await getAllSimpleTravels();
   return (
     <Container>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 place-items-center">
-        {tourList.length > 0 &&
-          tourList.map((currTour) => {
-            return <TourCard key={uuidv4()} tourElement={currTour} />;
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 place-items-center">
+        {toursList.length > 0 &&
+          toursList.map((currTour) => {
+            return <TourCard key={currTour.id} tourElement={currTour} />;
           })}
       </div>
     </Container>
