00fbb99413cb451c05b87934df297aa44b440daf
isaac <ishak.hadj14@gmail.com>
2025-09-06 17:28:45 +0100

    update the list with the new selected list component

--- src/component/molecules/ToursList.tsx
+++ src/component/molecules/ToursList.tsx
@@ -1,18 +1,13 @@
 import Container from "./Container";
 import { TourSimple } from "@/Types/Types";
 import { getAllSimpleTravels } from "@/_lib/apiClient";
-import TourCard from "./TourCard/TourCard";
+import SelectedTravels from "./SelectedTravels";
 
 const TourList = async () => {
   const toursList: TourSimple[] = await getAllSimpleTravels();
   return (
     <Container>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 place-items-center">
-        {toursList.length > 0 &&
-          toursList.map((currTour) => {
-            return <TourCard key={currTour.id} tourElement={currTour} />;
-          })}
-      </div>
+      <SelectedTravels travelItems={toursList} />
     </Container>
   );
 };
