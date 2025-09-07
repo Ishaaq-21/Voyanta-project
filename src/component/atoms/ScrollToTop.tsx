f668989ec0a4687160c1c345d4665b4787a20e1d
isaac <ishak.hadj14@gmail.com>
2025-09-07 22:44:40 +0100

    feat(scrollToTop): build a scrollToTop component that add a smooth scrolling on paginting

---
+++ src/component/atoms/ScrollToTop.tsx
@@ -0,0 +1,15 @@
+"use client";
+import { useEffect } from "react";
+import { useSearchParams } from "next/navigation";
+
+export default function ScrollToTop() {
+  const searchParams = useSearchParams();
+  const page = searchParams.get("page");
+
+  console.log(page);
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  }, [searchParams.toString()]); // runs on every route change
+
+  return null;
+}
