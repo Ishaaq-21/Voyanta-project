247724da0ed77be039e96081bc62bcf4c1fb96ea
isaac <ishak.hadj14@gmail.com>
2025-09-11 15:41:23 +0100

    refactor (TourGallery): build the  Gallery section for the tourDetails page

---
+++ src/component/organism/TourDetailsPage/Gallery.tsx
@@ -0,0 +1,44 @@
+import { TourSecProps } from "@/Types/Types";
+import Image from "next/image";
+
+const Gallery = ({ tourData }: TourSecProps) => {
+  return (
+    <section className="bg-slate-100 py-20 md:py-28">
+      <div className="container mx-auto px-4">
+        <div className="text-center mb-16">
+          <h2 className="text-4xl md:text-5xl font-extrabold bg-gradient-to-r from-amber-400 to-amber-700 bg-clip-text   text-transparent tracking-tight uppercase">
+            Field Notes & Gallery
+          </h2>
+          <p className="mt-3 text-lg text-slate-600 max-w-2xl mx-auto ">
+            A collection of waypoints, sights, and moments captured from the
+            trail.
+          </p>
+        </div>
+        <div className="grid  grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+          {tourData.images.map((imgPath, index) => {
+            return (
+              <div
+                key={imgPath}
+                className={`p-3 rounded-md shadow-2xl transform  max-h-52 ${
+                  index % 2 === 0
+                    ? "bg-slate-800 lg:-rotate-3"
+                    : "bg-dark-primary lg:rotate-3"
+                }`}
+              >
+                <Image
+                  src={imgPath}
+                  alt={imgPath}
+                  className="w-full h-full object-cover"
+                  width={120}
+                  height={120}
+                />
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Gallery;
