9e590c0aefd9d5c6c4cc51bcf2306a0a5de5e60a
isaac <ishak.hadj14@gmail.com>
2025-09-20 08:00:54 +0100

    style: change btn style

--- src/component/organism/Header/AuthBtn.tsx
+++ src/component/organism/Header/AuthBtn.tsx
@@ -1,12 +1,11 @@
 import MainButton from "@/component/atoms/MainButton";
-import clsx from "clsx";
 import Link from "next/link";
 import { JSX } from "react";
 
 //pirmary-btn (-small) are custom classes defined in the global.css @layer component
 const LoginBtn = ({ small }: { small: boolean }): JSX.Element => {
   return (
-    <MainButton as={Link} href={"/sign-in"} small={small}>
+    <MainButton as={Link} href={"/sing-in"} small={small}>
       Login
     </MainButton>
   );
