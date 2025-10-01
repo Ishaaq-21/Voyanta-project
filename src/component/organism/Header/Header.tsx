b9e1672b9596f411e1ba8c543a9c3ebf0c0f90f0
isaac <ishak.hadj14@gmail.com>
2025-10-01 15:56:37 +0100

    refactor: replace the user clerk button on signIn with our profile dashboard link button

--- src/component/organism/Header/Header.tsx
+++ src/component/organism/Header/Header.tsx
@@ -2,9 +2,10 @@ import Image from "next/image";
 import mainLogo from "../../../../public/logo-amber.png";
 import Link from "next/link";
 import Container from "../../molecules/Container";
-import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 import MobileNav from "./MobNav";
 import NavBar from "./Nav";
+import MainButton from "@/component/atoms/MainButton";
 const Header = () => {
   return (
     <header className="h-20 bg-gray-500 relative z-50">
@@ -18,19 +19,16 @@ const Header = () => {
             sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
           />
         </Link>
-        <Link
-          href={"/dashboard/user-profile"}
-          className="text-black font-semibold text-lg"
-        >
-          Dashboard
-        </Link>
+
         <div className="register">
           <SignedOut>
             <NavBar />
             <MobileNav />
           </SignedOut>
           <SignedIn>
-            <UserButton />
+            <MainButton as={Link} href={"/dashboard/user-profile"} small={true}>
+              Profile
+            </MainButton>
           </SignedIn>
         </div>
       </Container>
