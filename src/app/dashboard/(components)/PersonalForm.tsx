93a22e9ead3ca33b57f4fe05be1b9aafbe33ce9d
isaac <ishak.hadj14@gmail.com>
2025-10-03 21:22:47 +0100

    refactor: update form ui dynamically

--- src/app/dashboard/(components)/PersonalForm.tsx
+++ src/app/dashboard/(components)/PersonalForm.tsx
@@ -5,7 +5,12 @@ import Image from "next/image";
 const PersonalForm = () => {
   const userObj = useUser().user;
   return (
-    <form className="space-y-6">
+    <form className="space-y-6" onSubmit={handleSubmit}>
+      {message && (
+        <p className={`${message.error ? "text-red-500" : "text-green-500"}`}>
+          {message.message}
+        </p>
+      )}
       <div className="flex items-center space-x-4">
         <div className="w-16 h-16 rounded-full relative overflow-hidden bg-gray-200">
           <Image
@@ -24,15 +29,32 @@ const PersonalForm = () => {
       </div>
       <div>
         <label
-          htmlFor="name"
+          htmlFor="firstName"
           className="block text-sm font-medium text-gray-700"
         >
-          Name
+          First Name
         </label>
         <input
+          value={firstNameState}
+          onChange={(e) => setFirstNameState(e.target.value)}
           type="text"
-          id="name"
-          defaultValue={(userObj?.firstName as string) || ""}
+          id="firstName"
+          required
+          className="mt-1 block w-full px-4 py-3 bg-gray-50 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-amber-500 focus:border-amber-500 sm:text-sm"
+        />
+      </div>
+      <div>
+        <label
+          htmlFor="lastName"
+          className="block text-sm font-medium text-gray-700"
+        >
+          Last Name
+        </label>
+        <input
+          value={lastNameState}
+          onChange={(e) => setLastNameState(e.target.value)}
+          type="text"
+          id="lastName"
           required
           className="mt-1 block w-full px-4 py-3 bg-gray-50 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-amber-500 focus:border-amber-500 sm:text-sm"
         />
@@ -48,10 +70,10 @@ const PersonalForm = () => {
           type="email"
           id="email"
           disabled
+          required
           defaultValue={
             (userObj?.emailAddresses?.[0]?.emailAddress as string) || ""
           }
-          required
           className="mt-1 block  text-gray-500 w-full px-4 py-3 bg-gray-50 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-amber-500 focus:border-amber-500 sm:text-sm"
         />
       </div>
@@ -59,9 +81,9 @@ const PersonalForm = () => {
       <div className="text-right">
         <button
           type="submit"
-          className="inline-flex justify-center py-2 px-6 border border-transparent shadow-sm text-sm font-medium rounded-full text-white bg-amber-400 hover:bg-amber-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-amber-500 transition-transform hover:scale-105"
+          className="inline-flex justify-center py-2 px-6 border border-transparent shadow-sm text-sm font-medium rounded-full text-white bg-amber-400 hover:bg-amber-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-amber-500 transition-transform hover:scale-105 cursor-pointer"
         >
-          Save settings
+          {isLoading ? "Changing..." : "Change name"}
         </button>
       </div>
     </form>
