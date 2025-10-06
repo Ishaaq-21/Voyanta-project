d509c55b2f682598b331f6bf18ab2f561606fa88
isaac <ishak.hadj14@gmail.com>
2025-10-06 14:53:47 +0100

    finish the main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,27 @@
-import Footer from "@/component/organism/Footer";
-import Header from "../component/organism/Header";
+import TourList from "@/component/molecules/TourList/ToursList";
+import ToursSearch from "@/component/molecules/TourList/SearchFilter/SearchFilter";
+import { TourType } from "@/Types/Types";
 
-export default function Home() {
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<{
+    page?: string;
+    searchTerm?: string;
+    tourType?: TourType;
+  }>;
+}) {
+  const searchParamsRes = await searchParams;
+  const page = Number(searchParamsRes.page) || 1;
+  const searchTerm = searchParamsRes.searchTerm || "";
+  const tourType = searchParamsRes.tourType || "all";
+
   return (
     <>
-      <Header />
-      <main className="h-[calc(100vh-192px)]"></main>
-      <Footer />
+      <main className="min-h-[calc(100vh-176px)] relative pt-16 pb-20">
+        <ToursSearch />
+        <TourList currPage={page} searchTerm={searchTerm} tourType={tourType} />
+      </main>
     </>
   );
 }
